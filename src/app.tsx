5f2a1f775afd88aeff73511090d9d2f6a2abc118
Pedro Makoski <pedro.makoski.pedrogamail.com@gmail.com>
2025-07-08 20:34:06 -0300

    aula 1

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CreateRoom } from './pages/create-room';
+import { Room } from './pages/room';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
+
+export { App };
